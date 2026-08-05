44ba59ac8bc21ee2cced5bd5286d0f14901a7153
4lihmza1 <legendtriger@gmail.com>
2026-08-05 13:20:27 +0500

    Fix-onboarding-Compose-import

--- app/src/main/java/com/alihamza/iradex/Onboarding.kt
+++ app/src/main/java/com/alihamza/iradex/Onboarding.kt
@@ -1,4 +1,4 @@
-package com.alihamza.iradex
+﻿package com.alihamza.iradex
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
